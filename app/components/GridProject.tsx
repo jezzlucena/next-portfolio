601d5e8cdfa9f4628e13519d021265577265e0a4
Jezz Lucena <jezzlucena@Jezzs-MacBook-Air.local>
2023-11-14 12:10:58 -0800

    Add TailwindCSS Skeleton Loading for GridProject component lazy loading

--- app/components/GridProject.tsx
+++ app/components/GridProject.tsx
@@ -14,7 +14,7 @@ export default function GridProject(props: GridItemProps) {
         className="relative mb-3 mr-3 cursor-pointer group border border-gray-300 px-5 py-4 transition-colors hover:border-gray-700 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         onClick={props.onClick}
     >
-        {project && <>
+        {project ? <>
                 <h2 className={`mb-3 text-xl font-semibold`}>
                     {`${project.name} `}
                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -34,7 +34,15 @@ export default function GridProject(props: GridItemProps) {
                 <p className={`m-0 text-sm opacity-50`}>
                     {project.description}
                 </p>
-            </>
+            </> : <div role="status" className="max-w-sm animate-pulse">
+                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                <span className="sr-only">Loading...</span>
+            </div>
         }
     </div>
 }
